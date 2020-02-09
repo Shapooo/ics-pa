af11ef48d6d4b21e9a5126b67712b47bedb4ef13
Shapo <ShaboJia@gmail.com>
2020-02-09 22:41:09 +0800

    condition code completed modified:   nemu/src/isa/x86/exec/cc.c modified:   nemu/src/isa/x86/exec/exec.c

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -14,16 +14,38 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
-    default: panic("should not reach here");
-    case CC_P: panic("n86 does not have PF");
+  case CC_O:
+    rtl_get_OF(dest);
+    break;
+  case CC_B:
+    rtl_get_CF(dest);
+    break;
+  case CC_E:
+    rtl_get_ZF(dest);
+    break;
+  case CC_BE:
+    rtl_get_CF(&s0);
+    rtl_get_ZF(&s1);
+    rtl_or(dest, &s0, &s1);
+    break;
+  case CC_S:
+    rtl_get_SF(dest);
+    break;
+  case CC_L:
+    rtl_get_SF(&s0);
+    rtl_get_OF(&s1);
+    *dest = (s0 != s1);
+  case CC_LE:
+    /* TODO(); */
+    rtl_get_ZF(dest);
+    rtl_get_SF(&s0);
+    rtl_get_OF(&s1);
+    *dest |= (s0 != s1);
+    break;
+  default:
+    panic("should not reach here");
+  case CC_P:
+    panic("n86 does not have PF");
   }
 
   if (invert) {

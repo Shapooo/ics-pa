bd4a8f668cdfcbbe57b3f6e7515084ea81d53bbb
Shapo <ShaboJia@gmail.com>
2020-03-07 11:12:15 +0800

    modified:   nemu/src/isa/x86/diff-test.c: diff-test implemented

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,7 +2,21 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  /* return false; */
+  uint32_t *refRegP = (uint32_t*)ref_r;
+  uint32_t *locRegP = (uint32_t*)&cpu;
+  int i = 0;
+  for (; i < (DIFFTEST_REG_SIZE / sizeof(uint32_t)); ++i) {
+    if (refRegP[i] != locRegP[i]) {
+      printf("reg[%d] was not matched\n", i);
+      return false;
+    }
+  }
+  /* if ((refRegP[i] & 0x8c1) != (locRegP[i] & 0x8c1)) { */
+  /*   return false; */
+  /* } */
+  return true;
+
 }
 
 void isa_difftest_attach(void) {

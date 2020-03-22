01eebecde2cd171b40836e79f9e241d6626e845c
Shapo <ShaboJia@gmail.com>
2020-03-22 09:13:12 +0800

    modified:   nexus-am/am/src/nemu-common/nemu-input.c: keyboard

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -8,8 +8,9 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      uint32_t scancode = inl(KBD_ADDR);
+      kbd->keydown = ((scancode & KEYDOWN_MASK) ? 1 : 0);
+      kbd->keycode = scancode & (~KEYDOWN_MASK);
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }

fcc13ceea0ed3fd8c462ccacf74cc154e0ce51ec
Shapo <ShaboJia@gmail.com>
2020-03-29 22:53:40 +0800

    add irq modified:   nemu/src/isa/x86/exec/all-instr.h modified:   nemu/src/isa/x86/exec/exec.c modified:   nemu/src/isa/x86/exec/system.c modified:   nemu/src/isa/x86/intr.c modified:   nexus-am/am/include/arch/x86-nemu.h

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -1,9 +1,19 @@
+#include "cpu/decode.h"
 #include "rtl/rtl.h"
 
 void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  rtl_push(&cpu.eflags_val);
+  cpu.IF = 0;
+  cpu.TF = 0;
+  rtl_push(&cpu.cs);
+  rtl_push(&decinfo.seq_pc);
+#define IDT_ITEM_SIZE 8         /* 64 bits */
+  uint32_t hi = vaddr_read(cpu.idtr.base + NO * IDT_ITEM_SIZE + 6, 2);
+  uint32_t lo = vaddr_read(cpu.idtr.base + NO * IDT_ITEM_SIZE, 2);
+  rtl_j((hi << 16) | lo);
 }
 
 bool isa_query_intr(void) {

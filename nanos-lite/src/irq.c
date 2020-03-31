8d1f9a574b40f903a490dc56b87c0d34225ba0d7
Shapo <ShaboJia@gmail.com>
2020-03-31 17:43:48 +0800

    syscall...to be continued... modified:   src/irq.c modified:   src/syscall.c modified:   ../nexus-am/am/include/arch/x86-nemu.h modified:   ../nexus-am/am/src/x86/nemu/cte.c

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,11 +1,22 @@
 #include "common.h"
 
+extern _Context* do_syscall(_Context *c);
 static _Context* do_event(_Event e, _Context* c) {
+  _Context * ret;
   switch (e.event) {
+  case (_EVENT_YIELD): {
+    printf("Yield test...\n");
+    ret = NULL;
+    break;
+  }
+  case (_EVENT_SYSCALL): {
+    ret = do_syscall(c);
+    break;
+  }
     default: panic("Unhandled event ID = %d", e.event);
   }
 
-  return NULL;
+  return ret;
 }
 
 void init_irq(void) {
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,12 +2,25 @@
 #include "syscall.h"
 
 _Context* do_syscall(_Context *c) {
+  _Context *ret;
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
+  case (SYS_exit): {
+    _halt(a[1]);
+    break;
+  }
+  case (SYS_yield): {
+    _yield();
+    ret = 0;
+    break;
+  }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
-  return NULL;
+  return ret;
 }

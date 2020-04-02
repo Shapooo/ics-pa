453475f274fa7bd2fcd060719044fd9b882aa5c7
Shapo <ShaboJia@gmail.com>
2020-04-02 16:08:03 +0800

    file manipulation syscall/func finished: modified:   nanos-lite/src/fs.c modified:   nanos-lite/src/syscall.c modified:   navy-apps/libs/libos/src/nanos.c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "syscall.h"
 #include <stdint.h>
+
 inline static uintptr_t sys_exit(_Context *c) {
   uintptr_t exit_status = c->GPR2;
   _halt(exit_status);
@@ -12,8 +13,27 @@ inline static uintptr_t sys_yield(_Context *c) {
   return 0;
 }
 
+extern int fs_open(const char *pathname, int flags, int mode);
+extern size_t fs_read(int fd, void *buf, size_t len);
+extern size_t fs_write(int fd, const void *buf, size_t len);
+extern size_t fs_lseek(int fd, size_t offset, int whence);
+extern int fs_close(int fd);
 #define STDOUT 1
 #define STDERR 2
+
+inline static uintptr_t sys_open(_Context *c) {
+  const char *path = (const char *)c->GPR2;
+  int flags = (int)c->GPR3;
+  int mode = (int)c->GPR4;
+  return fs_open(path, flags, mode);
+}
+
+inline static uintptr_t sys_read(_Context *c) {
+  int fd = (int)c->GPR2;
+  void *buf = (void *)c->GPR3;
+  size_t count = (size_t)c->GPR4;
+  return fs_read(fd, buf, count);
+}
 
 inline static uintptr_t sys_write(_Context *c) {
   uintptr_t fd = c->GPR2;
@@ -25,6 +45,18 @@ inline static uintptr_t sys_write(_Context *c) {
     }
   }
   return count;
+}
+
+inline static uintptr_t sys_close(_Context *c) {
+  int fd = (int)c->GPR2;
+  return fs_close(fd);
+}
+
+inline static uintptr_t sys_lseek(_Context *c) {
+  int fd = (int)c->GPR2;
+  size_t offset = (size_t)c->GPR3;
+  int whence = (int)c->GPR4;
+  return fs_lseek(fd, offset, whence);
 }
 
 _Context *do_syscall(_Context *c) {
@@ -39,8 +71,24 @@ _Context *do_syscall(_Context *c) {
     c->GPRx = sys_yield(c);
     break;
   }
+  case (SYS_open): {
+    c->GPRx = sys_open(c);
+    break;
+  }
+  case (SYS_read): {
+    c->GPRx = sys_read(c);
+    break;
+  }
   case (SYS_write): {
     c->GPRx = sys_write(c);
+    break;
+  }
+  case (SYS_close): {
+    c->GPRx = sys_close(c);
+    break;
+  }
+  case (SYS_lseek): {
+    c->GPRx = sys_lseek(c);
     break;
   }
   case (SYS_brk): {
--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -54,13 +54,14 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
+  /* _exit(SYS_open); */
+  _syscall_(SYS_open, (uintptr_t)path, flags, mode);
   return 0;
 }
 
 int _write(int fd, void *buf, size_t count) {
   /* _exit(SYS_write); */
-  _syscall_(SYS_write, fd, buf, count);
+  _syscall_(SYS_write, fd, (uintptr_t)buf, count);
   return 0;
 }
 
@@ -79,17 +80,20 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _exit(SYS_read);
+  /* _exit(SYS_read); */
+  _syscall_(SYS_read, fd, (uintptr_t)buf, count);
   return 0;
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
+  /* _exit(SYS_close); */
+  _syscall_(SYS_close, fd, 0, 0);
   return 0;
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
+  /* _exit(SYS_lseek); */
+  _syscall_(SYS_lseek, fd, offset, whence);
   return 0;
 }
 

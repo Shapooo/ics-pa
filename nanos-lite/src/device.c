65e90364f9553dc3de268cdb2a8cfefeb08e3b69
Shapo <ShaboJia@gmail.com>
2020-04-03 16:58:54 +0800

    nanos-lite/src/device.c: /dev/events finished

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,8 +19,23 @@ static const char *keyname[256] __attribute__((used)) = {
   _KEYS(NAME)
 };
 
+/* FIXME: In navy-apps/tests/events test, fgetc get '\0' char
+ * every time:
+ * fgetc get one char only (len = 1), that makes snprintf
+ * put '\0' to output. */
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  size_t real_size;
+
+  int keycode = read_key();
+  int down = keycode >> 15;
+  keycode &= 0x7fff;
+  if (keycode != _KEY_NONE) {
+    real_size = snprintf(buf, len, "k%c %s", (down) ? 'd' : 'u', keyname[keycode]);
+  } else {
+    real_size = snprintf(buf, len, "t %d", uptime());
+  }
+  printf("%d\n", *(char *)buf);
+  return real_size;
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
